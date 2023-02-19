270664799a116831d2d9111574df4cbb038dca89
leesavage09 <leesavage09@gmail.com>
2023-02-19 18:08:31 +0200

    feat: added authorization middleware and signin signout api routes

---
+++ src/pages/api/signin.ts
@@ -0,0 +1,25 @@
+import { NextApiHandler } from 'next'
+import { setAuthCookie as getAuthCookie } from 'src/backend/api/auth'
+import { createResponse } from 'src/backend/api/response'
+import { connectMongo } from 'src/backend/db/connectMongo'
+import { loginUser } from 'src/backend/db/services/user.services'
+import { log } from 'src/backend/middleware/log'
+import { validate } from 'src/backend/middleware/validate'
+import { LoginSchema, loginSchema } from 'src/yup-schema/login'
+
+const handler: NextApiHandler = async (req, res) => {
+  await connectMongo()
+  const user = req.body as LoginSchema
+  const jwt = await loginUser(user.email, user.password)
+
+  if (jwt) {
+    res.setHeader('Set-Cookie', getAuthCookie(jwt))
+    return res.status(200).json(createResponse({ message: `Ok` }))
+  }
+
+  res
+    .status(401)
+    .json(createResponse(undefined, `Username or password incorrect`))
+}
+
+export default log(validate(loginSchema, handler))
---
+++ src/pages/api/signout.ts
@@ -0,0 +1,11 @@
+import { NextApiHandler } from 'next'
+import { setAuthCookie as getAuthCookie } from 'src/backend/api/auth'
+import { createResponse } from 'src/backend/api/response'
+import { log } from 'src/backend/middleware/log'
+
+const handler: NextApiHandler = async (req, res) => {
+  res.setHeader('Set-Cookie', getAuthCookie(null))
+  res.status(200).json(createResponse({ message: `Ok` }))
+}
+
+export default log(handler)
